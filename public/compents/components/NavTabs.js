bb1f4c776f50fcee6e973e42616ca0755e1c438c
lilliejack05 <49662301+lilliejack05@users.noreply.github.com>
2019-09-15 01:30:25 -0500

    added public files src components images installed packages

---
+++ public/compents/components/NavTabs.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function NavBar() {
+    return (<div className="App">
+        <nav className="navbar navbar-light bg-light">
+            <span className="navbar-brand mb-0 h1">Looney Toones Memory</span>
+        </nav>
+
+        
+    </div>
+
+    )
+
+};
+
+export default NavBar
